6e726e1afe1abd598503042504bc08d66574cf12
math4tots <math4tots@users.noreply.github.com>
2023-09-08 01:26:03 +0700

    mtots3: ...

--- mtots3/src/main.cc
+++ mtots3/src/main.cc
@@ -4,20 +4,25 @@ using namespace mtots;
 using namespace std;
 
 int main() {
-  auto str = String::make("Hello world!");
-  cout << str << endl;
+  try {
+    auto str = String::make("Hello world!");
+    cout << str << endl;
 
-  auto list = List::make({1.0, 2.0, 3.0});
-  cout << list << endl;
+    auto list = List::make({1.0, 2.0, 3.0});
+    cout << list << endl;
 
-  auto dict = Dict::make({
-      {1.0, 3.0},
-      {String::make("key"), String::make("value")},
-      {intern("key"), String::make("value")},
-  });
-  cout << "dict = " << dict << endl;
+    auto dict = Dict::make({
+        {1.0, 3.0},
+        {String::make("key"), String::make("value")},
+        {intern("key"), String::make("value")},
+    });
+    cout << "dict = " << dict << endl;
 
-  panic([](auto &out) {
-    out << "There was some error";
-  });
+    panic([](auto &out) {
+      out << "There was some error";
+    });
+  } catch (const Error &err) {
+    cerr << "PANIC: " << err.message << endl;
+    abort();
+  }
 }
--- mtots3/src/mtots.hh
+++ mtots3/src/mtots.hh
@@ -2,6 +2,7 @@
 #include <cstdlib>
 #include <functional>
 #include <iostream>
+#include <map>
 #include <sstream>
 #include <string>
 #include <unordered_map>
@@ -19,15 +20,23 @@ using u16 = std::uint16_t;
 using u32 = std::uint32_t;
 using u64 = std::uint64_t;
 
+class Error final {
+ public:
+  std::string message;
+  Error(const std::string &s) : message(s) {}
+  Error(std::string &&s) : message(std::move(s)) {}
+};
+
 [[noreturn]] inline void panic(std::function<void(std::ostream &)> f) {
-  f(std::cerr);
-  std::cerr << std::endl;
-  abort();
+  std::stringstream ss;
+  f(ss);
+  throw Error(ss.str());
 }
 
-[[noreturn]] inline void panic(std::string &&message) {
-  std::cerr << message << std::endl;
-  abort();
+[[noreturn]] inline void panic(const std::string &message) {
+  panic([&](auto &out) {
+    out << message;
+  });
 }
 
 class Symbol;
@@ -435,3 +444,256 @@ inline size_t hash<::mtots::Value>::operator()(const ::mtots::Value &value) cons
 }
 
 }  // namespace std
+
+namespace mtots {
+
+/**
+ * Literal class type that wraps a constant expression string.
+ *
+ * Uses implicit conversion to allow templates to *seemingly* accept constant strings.
+ *
+ * Delimiters, Operators and Keywords
+ */
+template <size_t N>
+struct Key final {
+  constexpr Key(const char (&str)[N]) {
+    std::copy_n(str, N, value);
+  }
+
+  char value[N];
+};
+
+// Explicit class template argument deduction guide
+// to suppress "-Wctad-maybe-unsupported" warnings
+template <size_t N>
+Key(const char (&str)[N]) -> Key<N>;
+
+class TokenBase {
+ public:
+  u32 line;
+  TokenBase(u32 ln) : line(ln) {}
+};
+
+/// @brief Token with no value field
+template <Key K>
+class KeyToken final : public TokenBase {
+ public:
+  static const char *getTypeName() {
+    return K.value;
+  }
+};
+
+template <Key K, class Arg>
+class ArgToken final : public TokenBase {
+ public:
+  Arg value;
+  ArgToken(u32 ln, Arg &&arg) : TokenBase(ln), value(std::move(arg)) {}
+};
+
+using EOFToken = KeyToken<"EOF">;
+using NewlineToken = KeyToken<"NEWLINE">;
+using IndentToken = KeyToken<"INDENT">;
+using DedentToken = KeyToken<"DEDENT">;
+
+using IdentifierToken = ArgToken<"IDENTIFIER", std::string>;
+using StringToken = ArgToken<"STRING", std::string>;
+using NumberToken = ArgToken<"NUMBER", double>;
+
+// List of tokens more or less as described in
+// https://docs.python.org/3/reference/lexical_analysis.html
+using Token = std::variant<
+    EOFToken,
+    NewlineToken,
+    IndentToken,
+    DedentToken,
+    IdentifierToken,
+
+    // Keywords
+    KeyToken<"False">,
+    KeyToken<"await">,
+    KeyToken<"else">,
+    KeyToken<"import">,
+    KeyToken<"pass">,
+    KeyToken<"None">,
+    KeyToken<"break">,
+    KeyToken<"except">,
+    KeyToken<"in">,
+    KeyToken<"raise">,
+    KeyToken<"True">,
+    KeyToken<"class">,
+    KeyToken<"finally">,
+    KeyToken<"is">,
+    KeyToken<"return">,
+    KeyToken<"and">,
+    KeyToken<"continue">,
+    KeyToken<"for">,
+    KeyToken<"lambda">,
+    KeyToken<"try">,
+    KeyToken<"as">,
+    KeyToken<"def">,
+    KeyToken<"from">,
+    KeyToken<"nonlocal">,
+    KeyToken<"while">,
+    KeyToken<"assert">,
+    KeyToken<"del">,
+    KeyToken<"global">,
+    KeyToken<"not">,
+    KeyToken<"with">,
+    KeyToken<"async">,
+    KeyToken<"elif">,
+    KeyToken<"if">,
+    KeyToken<"or">,
+    KeyToken<"yield">,
+
+    // Literals
+    StringToken,
+    NumberToken,
+
+    // Operators and Delimiters
+    KeyToken<"-">,
+    KeyToken<"-=">,
+    KeyToken<"->">,
+    KeyToken<",">,
+    KeyToken<";">,
+    KeyToken<":">,
+    KeyToken<":=">,
+    KeyToken<"!=">,
+    KeyToken<".">,
+    KeyToken<"(">,
+    KeyToken<")">,
+    KeyToken<"[">,
+    KeyToken<"]">,
+    KeyToken<"{">,
+    KeyToken<"}">,
+    KeyToken<"@">,
+    KeyToken<"@=">,
+    KeyToken<"*">,
+    KeyToken<"**">,
+    KeyToken<"**=">,
+    KeyToken<"*=">,
+    KeyToken<"/">,
+    KeyToken<"//">,
+    KeyToken<"//=">,
+    KeyToken<"/=">,
+    KeyToken<"&">,
+    KeyToken<"&=">,
+    KeyToken<"%">,
+    KeyToken<"%=">,
+    KeyToken<"^">,
+    KeyToken<"^=">,
+    KeyToken<"+">,
+    KeyToken<"+=">,
+    KeyToken<"<">,
+    KeyToken<"<<">,
+    KeyToken<"<<=">,
+    KeyToken<"<=">,
+    KeyToken<"=">,
+    KeyToken<"==">,
+    KeyToken<">">,
+    KeyToken<">=">,
+    KeyToken<">>">,
+    KeyToken<">>=">,
+    KeyToken<"|">,
+    KeyToken<"|=">,
+    KeyToken<"~">>;
+
+class Lexer final {
+  static auto keywords() {
+    static auto *map = new std::map<std::string, Token, std::less<>>({
+        {"False", Token(KeyToken<"False">{0})},
+        {"await", Token(KeyToken<"await">{0})},
+        {"else", Token(KeyToken<"else">{0})},
+        {"import", Token(KeyToken<"import">{0})},
+        {"pass", Token(KeyToken<"pass">{0})},
+        {"None", Token(KeyToken<"None">{0})},
+        {"break", Token(KeyToken<"break">{0})},
+        {"except", Token(KeyToken<"except">{0})},
+        {"in", Token(KeyToken<"in">{0})},
+        {"raise", Token(KeyToken<"raise">{0})},
+        {"True", Token(KeyToken<"True">{0})},
+        {"class", Token(KeyToken<"class">{0})},
+        {"finally", Token(KeyToken<"finally">{0})},
+        {"is", Token(KeyToken<"is">{0})},
+        {"return", Token(KeyToken<"return">{0})},
+        {"and", Token(KeyToken<"and">{0})},
+        {"continue", Token(KeyToken<"continue">{0})},
+        {"for", Token(KeyToken<"for">{0})},
+        {"lambda", Token(KeyToken<"lambda">{0})},
+        {"try", Token(KeyToken<"try">{0})},
+        {"as", Token(KeyToken<"as">{0})},
+        {"def", Token(KeyToken<"def">{0})},
+        {"from", Token(KeyToken<"from">{0})},
+        {"nonlocal", Token(KeyToken<"nonlocal">{0})},
+        {"while", Token(KeyToken<"while">{0})},
+        {"assert", Token(KeyToken<"assert">{0})},
+        {"del", Token(KeyToken<"del">{0})},
+        {"global", Token(KeyToken<"global">{0})},
+        {"not", Token(KeyToken<"not">{0})},
+        {"with", Token(KeyToken<"with">{0})},
+        {"async", Token(KeyToken<"async">{0})},
+        {"elif", Token(KeyToken<"elif">{0})},
+        {"if", Token(KeyToken<"if">{0})},
+        {"or", Token(KeyToken<"or">{0})},
+        {"yield", Token(KeyToken<"yield">{0})},
+    });
+    return &map;
+  }
+  static auto operators() {
+    static auto *map = new std::map<std::string, Token, std::less<>>({
+        {"-", Token(KeyToken<"-">{0})},
+        {"-=", Token(KeyToken<"-=">{0})},
+        {"->", Token(KeyToken<"->">{0})},
+        {",", Token(KeyToken<",">{0})},
+        {";", Token(KeyToken<";">{0})},
+        {":", Token(KeyToken<":">{0})},
+        {":=", Token(KeyToken<":=">{0})},
+        {"!=", Token(KeyToken<"!=">{0})},
+        {".", Token(KeyToken<".">{0})},
+        {"(", Token(KeyToken<"(">{0})},
+        {")", Token(KeyToken<")">{0})},
+        {"[", Token(KeyToken<"[">{0})},
+        {"]", Token(KeyToken<"]">{0})},
+        {"{", Token(KeyToken<"{">{0})},
+        {"}", Token(KeyToken<"}">{0})},
+        {"@", Token(KeyToken<"@">{0})},
+        {"@=", Token(KeyToken<"@=">{0})},
+        {"*", Token(KeyToken<"*">{0})},
+        {"**", Token(KeyToken<"**">{0})},
+        {"**=", Token(KeyToken<"**=">{0})},
+        {"*=", Token(KeyToken<"*=">{0})},
+        {"/", Token(KeyToken<"/">{0})},
+        {"//", Token(KeyToken<"//">{0})},
+        {"//=", Token(KeyToken<"//=">{0})},
+        {"/=", Token(KeyToken<"/=">{0})},
+        {"&", Token(KeyToken<"&">{0})},
+        {"&=", Token(KeyToken<"&=">{0})},
+        {"%", Token(KeyToken<"%">{0})},
+        {"%=", Token(KeyToken<"%=">{0})},
+        {"^", Token(KeyToken<"^">{0})},
+        {"^=", Token(KeyToken<"^=">{0})},
+        {"+", Token(KeyToken<"+">{0})},
+        {"+=", Token(KeyToken<"+=">{0})},
+        {"<", Token(KeyToken<"<">{0})},
+        {"<<", Token(KeyToken<"<<">{0})},
+        {"<<=", Token(KeyToken<"<<=">{0})},
+        {"<=", Token(KeyToken<"<=">{0})},
+        {"=", Token(KeyToken<"=">{0})},
+        {"==", Token(KeyToken<"==">{0})},
+        {">", Token(KeyToken<">">{0})},
+        {">=", Token(KeyToken<">=">{0})},
+        {">>", Token(KeyToken<">>">{0})},
+        {">>=", Token(KeyToken<">>=">{0})},
+        {"|", Token(KeyToken<"|">{0})},
+        {"|=", Token(KeyToken<"|=">{0})},
+        {"~", Token(KeyToken<"~">{0})},
+    });
+    return &map;
+  }
+
+  const std::string source;
+
+ public:
+  Lexer(std::string &&s) : source(std::move(s)) {}
+};
+
+}  // namespace mtots
