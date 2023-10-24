13e7ded5063cd572938e96f6677355797bb30fcd
math4tots <math4tots@users.noreply.github.com>
2023-10-24 23:06:24 +0700

    Delete all c.*Cell, c.*Slice stuff
    Don't need them so much anymore now that we have Pointer

--- src/mtots_m_c.c
+++ src/mtots_m_c.c
@@ -25,53 +25,6 @@ WRAP_C_FUNCTION_EX(mallocSizeof, MallocSizeof, 1, 2, {
 
 WRAP_C_FUNCTION_EX(free, Free, 1, 0, free(asVoidPointer(argv[0])))
 
-WRAP_PUBLIC_C_TYPE(IntCell, int)
-WRAP_C_FUNCTION_EX(get, IntCellGet, 0, 0, *out = valNumber(asIntCell(argv[-1])->handle))
-WRAP_C_FUNCTION_EX(set, IntCellSet, 0, 0, asIntCell(argv[-1])->handle = asInt(argv[0]))
-static CFunction *IntCellMethods[] = {
-    &funcIntCellGet,
-    &funcIntCellSet,
-    NULL,
-};
-
-WRAP_PUBLIC_C_TYPE(U16Cell, u16)
-WRAP_C_FUNCTION_EX(get, U16CellGet, 0, 0, *out = valNumber(asU16Cell(argv[-1])->handle))
-WRAP_C_FUNCTION_EX(set, U16CellSet, 0, 0, asU16Cell(argv[-1])->handle = asInt(argv[0]))
-static CFunction *U16CellMethods[] = {
-    &funcU16CellGet,
-    &funcU16CellSet,
-    NULL,
-};
-
-WRAP_PUBLIC_C_TYPE(U32Cell, u32)
-WRAP_C_FUNCTION_EX(get, U32CellGet, 0, 0, *out = valNumber(asU32Cell(argv[-1])->handle))
-WRAP_C_FUNCTION_EX(set, U32CellSet, 0, 0, asU32Cell(argv[-1])->handle = asInt(argv[0]))
-static CFunction *U32CellMethods[] = {
-    &funcU32CellGet,
-    &funcU32CellSet,
-    NULL,
-};
-
-static void blackenConstU8Slice(ObjNative *n) {
-  ObjConstU8Slice *arr = (ObjConstU8Slice *)n;
-  markValue(arr->handle.dataOwner);
-}
-WRAP_PUBLIC_C_TYPE_EX(ConstU8Slice, ConstU8Slice, blackenConstU8Slice, nopFree)
-WRAP_C_FUNCTION_EX(
-    __len__, ConstU8SliceLen, 0, 0,
-    *out = valNumber(asConstU8Slice(argv[-1])->handle.count))
-WRAP_C_FUNCTION_EX(__getitem__, ConstU8SliceGet, 1, 0, {
-  ObjConstU8Slice *array = asConstU8Slice(argv[-1]);
-  size_t index = asIndex(argv[0], array->handle.count);
-  *out = valNumber(array->handle.buffer[index]);
-})
-static CFunction *ConstU8SliceStaticMethods[] = {NULL};
-static CFunction *ConstU8SliceMethods[] = {
-    &funcConstU8SliceLen,
-    &funcConstU8SliceGet,
-    NULL,
-};
-
 static Status impl(i16 argc, Value *argv, Value *out) {
   ObjModule *module = asModule(argv[0]);
   CFunction *functions[] = {
@@ -83,10 +36,6 @@ static Status impl(i16 argc, Value *argv, Value *out) {
   };
 
   moduleAddFunctions(module, functions);
-  ADD_TYPE_TO_MODULE(IntCell);
-  ADD_TYPE_TO_MODULE(U16Cell);
-  ADD_TYPE_TO_MODULE(U32Cell);
-  ADD_TYPE_TO_MODULE(ConstU8Slice);
 
   WRAP_CONST(VOID, POINTER_TYPE_VOID);
   WRAP_CONST(CHAR, POINTER_TYPE_CHAR);
--- src/mtots_m_c.h
+++ src/mtots_m_c.h
@@ -5,17 +5,6 @@
 
 #include "mtots_macros_public.h"
 
-typedef struct ConstU8Slice {
-  const u8 *buffer;
-  size_t count;
-  Value dataOwner;
-} ConstU8Slice;
-
-DECLARE_PUBLIC_C_TYPE_PROTOTYPES(IntCell, int)
-DECLARE_PUBLIC_C_TYPE_PROTOTYPES(U16Cell, u16)
-DECLARE_PUBLIC_C_TYPE_PROTOTYPES(U32Cell, u32)
-DECLARE_PUBLIC_C_TYPE_PROTOTYPES(ConstU8Slice, ConstU8Slice)
-
 void addNativeModuleC(void);
 
 #endif /*mtots_m_c_h*/
